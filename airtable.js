5b859c49230c67fef4f64e4b4802424220ebd754
secureconnectdemo <112904935+secureconnectdemo@users.noreply.github.com>
2025-05-08 12:40:23 -0600

    Update airtable.js

--- airtable.js
+++ airtable.js
@@ -1,23 +1,3 @@
-const Airtable = require('airtable');
-require('dotenv').config();
-
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
-
-async function getAccountsWithBarrier(barrierType) {
-  const records = await base('Accounts')
-    .select({
-      filterByFormula: `{Barrier Type} = '${barrierType}'`,
-      fields: ['Account Name']
-    })
-    .firstPage();
-
-  return records.map(record => ({
-    title: record.get('Account Name') || 'Unnamed',
-    value: record.id
-  }));
-}
-
-module.exports = { getAccountsWithBarrier };
 const Airtable = require("airtable");
 require("dotenv").config();
 
